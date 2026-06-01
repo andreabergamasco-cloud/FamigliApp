ca9b3267ea967637a6f1152623ef10354eb6ba5f
Andrea Bergamasco <andrea.bergamasco@virevo.it>
2026-06-01 22:39:57 +0200

    .

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70091ed4fa8e8726422bcce77b60fd574d2a09f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
