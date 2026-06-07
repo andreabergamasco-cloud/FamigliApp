97118750bb3d3d9ae530d70b1bb83265f273586a
Andrea Bergamasco <andrea.bergamasco@virevo.it>
2026-06-07 19:26:03 +0200

    15 commit

--- backend/obj/Release/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Release/net8.0/backend.AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+718a600c0824c328aa4c2e6b3b2c4a7bfa72cb92")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
